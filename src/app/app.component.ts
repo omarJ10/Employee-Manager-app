e2bf0c9bc9abc8005f8d4295f8e8d06e0963d4d9
Jalled Omar <jalledomar2001@gmail.com>
2023-10-29 10:18:11 +0100

    Init angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,35 @@ import { Component, OnInit } from '@angular/core';
 import { Employee } from './employee';
 import { EmployeeService } from './employee.service';
 import { HttpErrorResponse } from '@angular/common/http';
-
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  public employees : Employee[] | undefined;
+  public employees: Employee[] = [];
+  public editEmployee: Employee | null = null;
+  public deleteEmployee: Employee | null = null;
+
+  defaultEmployee: Employee = {
+    id: 0, // Or some default value
+    name: '',
+    email :'',
+    phone : 0,
+    jobTitle :'',
+    imageUrl :'',
+    employeeCode :''
+
+  };
 
   ngOnInit(){
     this.getEmployees();
-    
   }
 
-  constructor(private employeeService: EmployeeService) { }
+  constructor(private employeeService: EmployeeService) {
+    this.employees = [];
+  }
   public getEmployees():void{
     this.employeeService.getEmployees().subscribe(
       (response: Employee[])=>{
@@ -27,4 +41,102 @@ export class AppComponent implements OnInit{
       }
     );
   }
+
+  public onAddEmloyee(addForm: NgForm): void {
+    if(document.getElementById('add-employee-form')){
+      document.getElementById('add-employee-form')?.click();
+    }
+
+    this.employeeService.addEmployee(addForm.value).subscribe(
+      (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+        addForm.reset();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+        addForm.reset();
+      }
+    );
+  }
+
+  public onDeleteEmloyee(employeeId: number | undefined): void {
+    if (employeeId === undefined) {
+      console.error('Invalid employee ID');
+      return;
+    }
+    this.employeeService.deleteEmployee(employeeId).subscribe(
+      (response: void) => {
+        console.log(response);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public searchEmployees(key: string): void {
+    console.log(key);
+    const results: Employee[] = [];
+    for (const employee of this.employees) {
+      if (employee.name.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || employee.email.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || employee.phone
+      || employee.jobTitle.toLowerCase().indexOf(key.toLowerCase()) !== -1) {
+        results.push(employee);
+      }
+    }
+    this.employees = results;
+    if (results.length === 0 || !key) {
+      this.getEmployees();
+    }
+  }
+
+
+  public onUpdateEmloyee(employee: Employee): void {
+    this.employeeService.updateEmployee(employee).subscribe(
+      (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+
+
+  public onOpenModal(employee: Employee | null, mode: string): void {
+    if (!employee) {
+      // Handle the case where employee is null or undefined
+      console.log('Employee is null or undefined');
+    }
+
+    const container = document.getElementById('main-container');
+    console.log('heloo');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle','modal');
+    console.log("button created",button);
+
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addEmployeeModal');
+    }
+    if (mode === 'edit') {
+      this.editEmployee = employee;
+      button.setAttribute('data-target', '#updateEmployeeModal');
+    }
+    if (mode === 'delete') {
+      this.deleteEmployee = employee;
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+    if (container) {
+      container.appendChild(button);
+      button.click();
+    }
+  }
+
 }
